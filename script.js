de5c7770b9a863b4d933a37667584ed0a3d9a156
Piotr Obara <pobara@o2.pl>
2023-06-01 20:10:25 +0200

    hamburger menu new version

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-const burgerBtn = document.querySelector('.hamburger_icon');
-burgerBtn.addEventListener("click", addclass)
+const Btn = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
 
-const burgerline1 = document.querySelector('.line1');
-const burgerline2 = document.querySelector('.line2');
-const burgerline3 = document.querySelector('.line3');
-const menu = document.querySelector('.menu');
-
-function addclass () { 
-
-    burgerline1.classList.toggle('line1_active');
-    burgerline2.classList.toggle('line2_active');
-    burgerline3.classList.toggle('line3_active');
-    menu.classList.toggle('unactive');
+function active() {
+	Btn.classList.toggle('hamburger-active');
+	nav.classList.toggle('active');
 }
 
-
-
-
+Btn.addEventListener('click', active);
